364c52f7853320025f64c70e8df9a4e76579a858
Hannah Nordgren <hey@hereshannahs.info>
2015-06-10 23:02:04 -0400

    trying callback

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(process.env.port || 8000);
 console.log('the magic happens on port 8000');
